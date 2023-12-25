203c09ff258099cdbb5cc949537bd83c916a00f9
Pepesi884748 <personiklaboratory@gmail.com>
2023-12-25 20:33:47 +0300

    fr

--- AndroidStudioProjects/gistogramma/app/src/main/java/com/example/gistogramma/MainActivity.java
+++ AndroidStudioProjects/gistogramma/app/src/main/java/com/example/gistogramma/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.gistogramma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Количество шекелей");
+
+        Context context = null;
+        BarChart chart = new BarChart(context);
+        setContentView(chart);
+
+        BarData data = new BarData(labels, dataset);
+        chart.setData(data);
+
     }
 }
